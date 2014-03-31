c4baf1c6ed2aac86437eac6848e762897a64a2de
dmkaplan2000 <dmkaplan2000@users.noreply.github.com>
2014-03-31 09:56:15 +0200

    Including DMK's fetch method

--- R/class.R
+++ R/class.R
@@ -6,7 +6,7 @@ setClass("H2Result", contains = "JDBCResult")
 # H2("RH2", jar = NULL, morePaths = "C:/tmp2/h2-1.3.155.jar") can be used
 # to specify a different jar file.
 H2 <- function(driverClass='org.h2.Driver', 
-  identifier.quote="\"", jars = getOption("RH2.jars"), ...) {
+               identifier.quote="\"", jars = getOption("RH2.jars"), ...) {
   #identifier.quote="\"", parameters = "-Dh2.identifiersToUpper=false", ...) {
   # options(java.parameters=parameters)
   if (is.null(jars)) jars <- "*"
@@ -20,11 +20,11 @@ H2 <- function(driverClass='org.h2.Driver',
 }
 
 setMethod("dbConnect", "H2Driver", def=function(drv, url = "jdbc:h2:mem:", 
-  user='sa', password='', DATABASE_TO_UPPER = getOption("RH2.DATABASE_TO_UPPER"), ...) {
+                                                user='sa', password='', DATABASE_TO_UPPER = getOption("RH2.DATABASE_TO_UPPER"), ...) {
   if (is.null(DATABASE_TO_UPPER) ||
-		(is.logical(DATABASE_TO_UPPER) && !DATABASE_TO_UPPER) ||
-		(is.character(DATABASE_TO_UPPER) && DATABASE_TO_UPPER == "FALSE"))
-			url <- paste(url, "DATABASE_TO_UPPER=FALSE", sep = ";")
+        (is.logical(DATABASE_TO_UPPER) && !DATABASE_TO_UPPER) ||
+        (is.character(DATABASE_TO_UPPER) && DATABASE_TO_UPPER == "FALSE"))
+    url <- paste(url, "DATABASE_TO_UPPER=FALSE", sep = ";")
   jc <- .jcall("java/sql/DriverManager","Ljava/sql/Connection;","getConnection", as.character(url)[1], as.character(user)[1], as.character(password)[1], check=FALSE)
   if (is.jnull(jc) || !is.jnull(drv@jdrv)) {
     # ok one reason for this to fail is its interaction with rJava's
@@ -37,7 +37,7 @@ setMethod("dbConnect", "H2Driver", def=function(drv, url = "jdbc:h2:mem:",
   }
   .verify.JDBC.result(jc, "Unable to connect JDBC to ",url)
   new("H2Connection", jc=jc, identifier.quote=drv@identifier.quote)},
-          valueClass="H2Connection")
+  valueClass="H2Connection")
 
 setMethod("dbWriteTable", "H2Connection", def=function(conn, name, value, overwrite=TRUE, ...) {
   dots <- list(...)
@@ -61,7 +61,7 @@ setMethod("dbWriteTable", "H2Connection", def=function(conn, name, value, overwr
   # cat("conn@identifier.quote:", conn@identifier.quote, "\n")
   qname <- .sql.qescape(name, FALSE, conn@identifier.quote)
   ct <- paste(if (temporary) "CREATE TEMPORARY TABLE" else "CREATE TABLE ",
-	      qname," (",fdef,")",sep= '')
+              qname," (",fdef,")",sep= '')
   # cat("ct:", ct, "\n")
   if (ac) {
     .jcall(conn@jc, "V", "setAutoCommit", FALSE)
@@ -70,20 +70,20 @@ setMethod("dbWriteTable", "H2Connection", def=function(conn, name, value, overwr
   dbSendUpdate(conn, ct)
   if (length(value[[1]])) {
     inss <- paste("INSERT INTO ",qname," VALUES(", paste(rep("?",length(value)),collapse=','),")",sep='')
-	# send Date variables as character strings
-	is.Date <- sapply(value, inherits, what = "Date")
-	for(i in which(is.Date)) {
-		value[[i]] <- as.character(value[[i]])
-	}
-	# send times variables as character strings
-	is.times <- sapply(value, function(x) identical(class(x), "times"))
-	for(i in which(is.times)) {
-		value[[i]] <- as.character(value[[i]])
-	}
-	if (NCOL(value) > 0) {
-		for (j in seq_along(value[[1]]))
-		  dbSendUpdate(conn, inss, list=as.list(value[j,]))
-	}
+    # send Date variables as character strings
+    is.Date <- sapply(value, inherits, what = "Date")
+    for(i in which(is.Date)) {
+      value[[i]] <- as.character(value[[i]])
+    }
+    # send times variables as character strings
+    is.times <- sapply(value, function(x) identical(class(x), "times"))
+    for(i in which(is.times)) {
+      value[[i]] <- as.character(value[[i]])
+    }
+    if (NCOL(value) > 0) {
+      for (j in seq_along(value[[1]]))
+        dbSendUpdate(conn, inss, list=as.list(value[j,]))
+    }
   }
   if (ac) dbCommit(conn)            
 })
@@ -93,66 +93,76 @@ setMethod("dbDataType", signature(dbObj="H2Connection", obj = "ANY"),
             if (is.integer(obj)) "INTEGER"
             else if (inherits(obj, "Date")) "DATE"
             else if (identical(class(obj), "times")) "TIME"
-			else if (inherits(obj, "POSIXct")) "TIMESTAMP"
+            else if (inherits(obj, "POSIXct")) "TIMESTAMP"
             else if (is.numeric(obj)) "DOUBLE PRECISION"
             else "VARCHAR(255)"
           }, valueClass = "character")
 
 setMethod("fetch", signature(res="H2Result", n="numeric"), def=function(res, n, ...) {
+  # Column count
   cols <- .jcall(res@md, "I", "getColumnCount")
-  if (cols < 1) return(NULL)
+  
+  # Row count  
+  nc <- .jcall(res@jr, "I", "getRow")
+  .jcall(res@jr, "Z", "absolute",as.integer(-1)) # Last row
+  nl <- .jcall(res@jr, "I", "getRow")
+  .jcall(res@jr, "Z", "absolute",as.integer(nc)) # Return to previous row
+  nn <- nl - nc # Remaining rows
+  if (n<0 | nn<n) n <- nn
+  
+  # Get column names
+  cns <- sapply( 1:cols,function(i) .jcall(res@md, "S", "getColumnName", i) )
+  
+  # Get column types
+  cts <- sapply( 1:cols,function(i) .jcall(res@md, "I", "getColumnType", i) )
+  
+  # Corresponding R data types
+  rts <- rep("character",cols)
+  I = which(cts == -5 | cts ==-6 | (cts >= 2 & cts <= 8))
+  rts[I] <- "numeric"
+  I = which(cts == 91)
+  rts[I] <- "Date"
+  I = which(cts == 92)
+  rts[I] <- "times"
+  I = which(cts == 93)
+  rts[I] <- "POSIXct"
+  
+  jts <- c(numeric="getDouble",character="getString",Date="getString",
+           times="getString",POSIXct="getString")[rts]
+  jnis <- c(numeric="D",character="S",Date="S",times="S",POSIXct="S")[rts] 
+  pps <- c(numeric=as.numeric,character=as.character,Date=as.Date,times=times,
+           POSIXct=as.POSIXct)[rts]
+  
+  # Pre-create result list
   l <- list()
-  for (i in 1:cols) {
-    ct <- .jcall(res@md, "I", "getColumnType", i)
-    l[[i]] <- if (ct == -5 | ct ==-6 | (ct >= 2 & ct <= 8)) { 
-      numeric()
-    } else if (ct == 91) { 
-      structure(numeric(), class = "Date")
-    } else if (ct == 92) { 
-      structure(numeric(), class = "times")
-    } else if (ct == 93) { 
-      structure(numeric(), class = class(Sys.time()))
-    } else character()
-    names(l)[i] <- .jcall(res@md, "S", "getColumnName", i)
+  for (i in 1:cols) l[[i]] = (pps[[i]])(rep(NA,n))
+  
+  names(l) <- cns
+  
+  if (n==0) return(as.data.frame(l,stringsAsFactors=FALSE))
+  
+  for (j in 1:n) {
+    if (j %% 1000==0) print(paste("Retrieving row",j))
+    if (!(.jcall(res@jr, "Z", "next")))
+      stop("Row not found") # Should never happen
+    
+    for (i in 1:cols) {
+      val <- .jcall(res@jr, jnis[[i]], jts[[i]], i)
+      if (!(.jcall(res@jr, "Z", "wasNull"))) l[[i]][j] <- (pps[[i]])(val)
+    }
   }
   
-  j <- 0
-  while (.jcall(res@jr, "Z", "next")) {
-    j <- j + 1
-    for (i in 1:cols) {
-      if (inherits(l[[i]], "Date")) {
-        # browser() ##
-        val <- .jcall(res@jr, "S", "getString", i)
-        wn <- .jcall(res@jr, "Z", "wasNull")
-        l[[i]][j] <- if (wn | is.null(val)) NA else as.Date(val)
-      } else if (inherits(l[[i]], "times")) {
-        val <- .jcall(res@jr, "S", "getString", i)
-        wn <- .jcall(res@jr, "Z", "wasNull")
-        l[[i]][j] <- if (wn | is.null(val)) NA else times(val)
-      } else if (inherits(l[[i]], "POSIXct")) {
-        val <- .jcall(res@jr, "S", "getString", i)
-        wn <- .jcall(res@jr, "Z", "wasNull")
-        l[[i]][j] <- if (wn | is.null(val)) NA else as.POSIXct(val)
-      } else if (is.numeric(l[[i]])) { 
-        val <- .jcall(res@jr, "D", "getDouble", i)
-        wn <- .jcall(res@jr, "Z", "wasNull")
-        l[[i]][j] <- if (wn | is.null(val)) NA else val
-      } else {
-        val <- .jcall(res@jr, "S", "getString", i)
-        wn <- .jcall(res@jr, "Z", "wasNull")
-        l[[i]][j] <- if (wn | is.null(val)) NA else val
-      }
-    }
-    if (n > 0 && j >= n) break
-  }
-  if (j)
-    as.data.frame(l, row.names=1:j)
-  else
-    as.data.frame(l)
+  # Just changes attributes to avoid large copy
+  attr(l, 'class') <- 'data.frame'
+  attr(l, "row.names") <- c(NA_integer_,n)
+  return(l)
 })
 
 setMethod("dbSendQuery", signature(conn="H2Connection", statement="character"),  def=function(conn, statement, ..., list=NULL) {
-  s <- .jcall(conn@jc, "Ljava/sql/PreparedStatement;", "prepareStatement", as.character(statement)[1], check=FALSE)
+  s <- .jcall(conn@jc, "Ljava/sql/PreparedStatement;", "prepareStatement", as.character(statement)[1],
+              as.integer(.jfield("java/sql/ResultSet","I","TYPE_SCROLL_INSENSITIVE")), 
+              as.integer(.jfield("java/sql/ResultSet","I","CONCUR_READ_ONLY")), 
+              check=FALSE)
   .verify.JDBC.result(s, "Unable to execute JDBC statement ",statement)
   if (length(list(...))) .fillStatementParameters(s, list(...))
   if (!is.null(list)) .fillStatementParameters(s, list)
